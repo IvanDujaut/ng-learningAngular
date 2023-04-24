2dc75ac066026ddb5b24796ccf1e9044c5e63762
Ivan Dujaut <ivan.dujaut@garajedeideas.com>
2023-04-23 21:46:18 -0300

    [NEW] - Ciclo de vida de un componente aplicado al componente img

--- src/app/components/img/img.component.ts
+++ src/app/components/img/img.component.ts
@@ -1,19 +1,49 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, OnChanges, AfterViewInit, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'app-img',
   templateUrl: './img.component.html',
   styleUrls: ['./img.component.scss']
 })
-export class ImgComponent implements OnInit {
+export class ImgComponent implements OnInit, OnChanges, AfterViewInit, OnDestroy {
 
   @Input() imgValueChild: string = '';
   @Output() loaded = new EventEmitter<string>();
   imageDefault = './assets/images/default.png';
 
-  constructor() { }
+  constructor() {
+    // before render
+    // NO async -- once time
+    console.log('constructor', 'imgValue =>' ,this.imgValueChild);
+
+  }
+
+  ngOnChanges(): void {
+    // before - during render
+    // changes inputs -- N times
+    console.log('ngOnChanges', 'imgValue =>', this.imgValueChild);
+
+  }
 
   ngOnInit(): void {
+    // before render
+    // async - fetch -- once time
+    console.log('ngOnInit', 'imgValue =>', this.imgValueChild);
+
+  }
+
+  ngAfterViewInit(): void {
+    // after render
+    // handler children. Manejamos los hijos de este componente de forma
+    // programatica y no desde el template (<ng-template>). Debido a que
+    // este componente ya se encuentra renderizado. Generalmente se ejecutan directivas
+    console.log('ngAfterViewInit');
+
+  }
+
+  ngOnDestroy(): void {
+    // delete
+    console.log('ngOnDestroy');
   }
 
   public imgError() {
