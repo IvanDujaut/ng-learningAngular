56c8cf7d70c1d99195248a8b2c0c088d9a73b66b
Ivan Dujaut <ivan.dujaut@garajedeideas.com>
2023-04-30 18:58:01 -0300

    [NEW] - Our own pipe was created, it is about change vowels by numbers

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,29 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Product } from '../../models/product.model'
-import { StoreService } from '../../services/store.service'
-import { ProductsService } from '../../services/products.service'
+import { Product } from '../../models/product.model';
+import { StoreService } from '../../services/store.service';
+import { ProductsService } from '../../services/products.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.scss']
+  styleUrls: ['./products.component.scss'],
 })
 export class ProductsComponent implements OnInit {
-
   public myShoppingCart: Product[] = [];
   public total = 0;
+  public today = new Date();
+  public otherDay = new Date(2021, 10, 28);
 
   public products: Product[] = [];
 
-  constructor(private storeService: StoreService, private productsService: ProductsService) {
+  constructor(
+    private storeService: StoreService,
+    private productsService: ProductsService
+  ) {
     this.myShoppingCart = this.storeService.getMyShoppingCart();
   }
 
   ngOnInit(): void {
     this.productsService.getAllProducts().subscribe((data) => {
       this.products = data;
-    })
+    });
   }
 
   onAddToShoppingCart(product: Product) {
